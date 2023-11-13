f0229351c6af7113bb25b879199eb519b0be50d3
ferea <tatamitina19@gmail.com>
2023-11-14 00:08:55 +0300

    final main page(3)

--- src/main/java/com/itmo/weblab3/beans/PointBean.java
+++ src/main/java/com/itmo/weblab3/beans/PointBean.java
@@ -9,6 +9,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import com.itmo.weblab3.model.CheckManagerInterface;
+import org.primefaces.PrimeFaces;
 
 import java.io.Serializable;
 import java.util.ArrayList;
